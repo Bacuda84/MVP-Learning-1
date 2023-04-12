cb71f99cf5160e91b72642d88f4053e8d862cc99
bacuda <appxcode450@gmail.com>
2023-04-12 18:20:26 +0700

    Initial Commit

---
+++ MVP-Learning-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP-Learning-1
+//
+//  Created by Bakhtovar Akhmedov on 12.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MVP-Learning-1UITests/MVP_Learning_1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MVP_Learning_1UITestsLaunchTests.swift
+//  MVP-Learning-1UITests
+//
+//  Created by Bakhtovar Akhmedov on 12.04.2023.
+//
+
+import XCTest
+
+final class MVP_Learning_1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
